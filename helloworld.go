05355616153127c3be95c4f603b5de458019e9e2
Tejas <tejaskothari03@gmail.com>
2024-10-13 04:41:25 +0530

    go helloworld init changes

---
+++ helloworld.go
@@ -0,0 +1,11 @@
+// Ref link : https://gosamples.dev/main-function-package-file/
+// go run helloworld.go --> TO RUN
+package main
+
+import "fmt"
+
+// main package -> main function ie entry point of execution
+func main() {
+
+	fmt.Println("hello World")
+}
